d536a2c7b1e9b73a25ba9eea691b76b983e37b3d
HongHyesun <hyessun@HongHyesunui-MacBook-Pro.local>
2017-06-25 21:29:07 +0900

    update

--- elections/views.py
+++ elections/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Candidate, Poll, Choice
+
+import datetime
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello_World")
+	candidate = Candidate.objects.all()
+	context = {'candidates': candidates}
+	return render(request, 'elections/index.html', context)
+
+def areas(request, area):
+	today = datetime.datetime.now()
+	try:
+		poll = Poll.objects.get(area = area, start_date__lte=
+			today, end_date__gte=today)
+		candidates = Candidate.objects.filter(area = area)
+	except:
+		poll = None
+		candidates = None	
+	context = {'candidates': candidates,
+	'area': area,
+	'poll': poll}
+	return render(request,'elections/area.html',context)
+
+	#str = ''
+	#for candidate in candidate:
+	#	str += "{} 기호 {}번 ({})<br>".format(candidate.name,
+	#		candidate.party_number,
+	#		candidate.area)
+	#	str += candidate.introduction+"</p>"
+
+	#return HttpResponse(str)
+	
