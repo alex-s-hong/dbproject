6a39d46337297baa2ae54b5acf13a2ad8a6963e2
Alex S Hong <alexsungmin.hong@gmail.com>
2017-06-26 10:22:02 +0000

    b4 dinner

--- elections/urls.py
+++ elections/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^areas/?P<area>.+)/$',views.areas)
+    url(r'^areas/?P<area>.+)/$',views.areas),
+    url(r'^areas/?P<area>.+)/results$',views.results),
     url(r'^polls/(?<poll_id>\d+)/$)',views.polls)
 ]
--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Candidate, Poll, Choice
 
 import datetime
@@ -38,7 +38,28 @@ def polls(request,poll_id):
 		choice = Choice(poll_id = poll_id, candidate_id = selection, votes = 1)
 		choice.save()
 
-	return HttpResponse("finish")
+	return HttpResponseRedirect("/areas/{}/results".format(poll.area))
+
+def results(request, area):
+    candidates = Candidate.objects.filter(area = area)
+
+    polls = Poll.objects.filter(area = area)
+    #poll과 result 합쳐서 하나로 표현하기 
+    poll_results = []
+    for poll in polls:
+        result = {}
+        result['start_date'] = poll.start_date
+        result['end_date'] = poll.end_date
+        
+        poll_results.append(result)
+   
+    context = {'candidates': candidates, 'area' :area,
+    'poll_results': poll_results}     
+        
+    return render(request, 'elections/result.html', context)
+
+
+
 
 	#str = ''
 	#for candidate in candidate:
